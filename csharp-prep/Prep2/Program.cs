eada69002a0e53c097e2054bf7cce58aaa78dde6
TMThulin <torgnymthulin@gmail.com>
2026-01-11 15:49:24 -0700

    completing developer parts 1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string gradeLetter = "";
+        string returnString = "";
+        string goodJob = "Congadulations, keep up the good work!";
+        string badJob = "Work hard and stay consistant, you can do this!";
+
+        Console.Write("What is your grade? ");
+        string gradeString = Console.ReadLine();
+
+        int grade = int.Parse(gradeString);
+
+        if (grade>90)
+        {
+            gradeLetter = "A";
+        }
+        else if (grade>80)
+        {
+            gradeLetter = "B";
+        }
+        else if (grade>70)
+        {
+            gradeLetter = "C";
+        }
+        else if (grade>60)
+        {
+            gradeLetter = "D";
+        }
+        else
+        {
+            gradeLetter = "F";
+        }
+
+        if (grade>70)
+        {
+            returnString = goodJob;
+        }
+        else
+        {
+            returnString = badJob;
+        }
+
+        Console.WriteLine($"Your grade is {gradeLetter}. {returnString}");
     }
 }
