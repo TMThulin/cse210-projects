d9168f701395dc8068bf877c894805a606757824
TMThulin <torgnymthulin@gmail.com>
2026-01-13 23:34:41 -0700

    completed prep project 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int response = 0;
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+
+        Console.WriteLine("please guess an integer from 1 to 100.");
+
+        while (response != number)
+        {
+            response= int.Parse(Console.ReadLine());
+
+            if (response<number)
+        {
+            Console.WriteLine("higher");
+        }
+        else if (response>number)
+        {
+            Console.WriteLine("lower");
+        }
+        else
+        {
+            Console.WriteLine("correct");
+        }
+        }
     }
 }
