e230177a9fe2af58b53114df0e3bcde80dd0caf0
TMThulin <torgnymthulin@gmail.com>
2026-01-15 02:31:12 -0700

    Completed prep project four

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<float> numbers = new List<float>();
+
+        float response = -1;
+        float largestNum = -2147483648;
+        float average = 0;
+        float total = 0;
+
+        while (response != 0)
+        {
+            Console.WriteLine("Please enter a number. Enter 0 to finish.");
+            response = float.Parse(Console.ReadLine());
+
+            if (response != 0)
+            {
+                numbers.Add(response);
+            }
+        }
+
+        total = numbers.Sum();
+        average = total / numbers.Count;
+
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            if (numbers[i] > largestNum)
+            {
+                largestNum = numbers[i];
+            }
+        }        
+        Console.WriteLine($"The list total is {total}. The list's average is {average}. The highest number in the list is {largestNum}");
     }
 }
